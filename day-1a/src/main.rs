db810f124e086c9218d3a5300d9ad97144fe3b74
Lester <lester.houtenbos@fourstroke.io>
2021-12-01 18:38:28 +0100

    Solution day-1a.

---
+++ day-1a/src/main.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+fn main() {
+
+    let file = fs::read_to_string("input.txt")
+        .expect("Something went wrong reading the file");
+    
+    let mut increases = 0;
+    let mut prev = i32::MAX;
+    for line in file.lines() {
+        let value = line.parse::<i32>().unwrap();
+        if value > prev {
+            increases += 1;
+        }
+        prev = value;
+    }
+
+    println!("Submarine depth increased {} times", increases);
+}
